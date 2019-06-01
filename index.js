336dca2c4106e7201cabf2e2e08d69d84a1317a6
Vinioo <vi.ni.cius4@hotmail.com>
2019-06-01 14:50:18 -0300

    Node JWT Authentication

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+
+// Import Routes
+const authRoute = require('./routes/auth');
+const postRoute = require('./routes/posts')
+
+dotenv.config();
+
+
+// Connect to DB
+mongoose.connect(process.env.DB_CONNECT, { useNewUrlParser: true }, () => console.log('Connected to DB!'))
+
+// Middlewares
+app.use(express.json());
+
+// Route Middlewares
+app.use('/api/user', authRoute);
+app.use('./api/post', postRoute)
+
+app.listen(3000, () => console.log('Server up and running'))
